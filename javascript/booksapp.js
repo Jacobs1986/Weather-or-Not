8d8a8a2d00a59b29dbdc0e8461fd86a57c66aa41
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-23 14:45:36 -0700

    Created for loop
    I decided to abandon the forEach loop and created a for-loop instead. It is working a lot better and displaying what I want.

--- javascript/booksapp.js
+++ javascript/booksapp.js
@@ -8,8 +8,9 @@ $.ajax({
     method: "GET"
 }).then(function(fictionBookList) {
     let list = fictionBookList.results.books;
-    typeofList = "fiction";
-    list.forEach(bookDisplay);
+    for (let i = 0; i < list.length; i++) {
+        bookDisplay(list[i], "fiction");
+    }
 })
 
 // nonfiction list
@@ -19,9 +20,13 @@ $.ajax({
     url: nonFictionURL,
     method: "GET"
 }).then(function(nonFictionBookList) {
-
+    let list = nonFictionBookList.results.books;
+    for (let i = 0; i < list.length; i++) {
+        bookDisplay(list[i], "nonfiction");
+    }
 })
 
 let bookDisplay = (list, typeofList) => {
     console.log(list.title);
+    console.log(typeofList);
 }
