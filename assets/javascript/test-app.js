7477831ff887f660f5c5dd30845dfc2293136d8a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-22 18:56:15 -0700

    Removed assets folder. Will add one again later.

--- assets/javascript/test-app.js
+++
@@ -1,8 +0,0 @@
-/*
-Just some thoughts on how I want this app to work
-I think that aside from the history of the weather for the gps there should be displayed activities the user can do depending on the weather
-Like for example I want there to be a list of books, video games and/or movies that will display when the chance of rain is >= 70%
-On the other hand I want the app to show the user some hiking trails, biking trails or running routes when the chance is <= 50%
-If the app shows the user books or video games I want there to be a list of some of the more popular/tranding items that they can select and in a card display show a picture, title, possibly rating as well as a review of said item.
-I think that the list should also have some checkbox options. Like if they want to see only books to select the box.
-*/
