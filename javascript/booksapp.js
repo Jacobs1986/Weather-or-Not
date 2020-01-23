4ec7b7e92c491ce21991a8814edb838079cdceab
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-23 15:04:05 -0700

    Wrote title
    Wrote the information needed to get and display the title for each of the books. Created a class and appended them to the listFiction div.

--- javascript/booksapp.js
+++ javascript/booksapp.js
@@ -9,7 +9,7 @@ $.ajax({
 }).then(function(fictionBookList) {
     let list = fictionBookList.results.books;
     for (let i = 0; i < list.length; i++) {
-        bookDisplay(list[i], "fiction");
+        bookDisplay(list[i], "listFiction");
     }
 })
 
@@ -32,11 +32,17 @@ let bookDisplay = (list, displayID) => {
     console.log(list);
     console.log(displayID);
     // create the book card where all the information is going to go, this will be a div with a class of .bookCard
+    let book = $("<div>").addClass("bookCard");
     // get the title, author, description and book_image and save them to different variables
+    let title = list.title;
     // create paragraph for the title, author and description
-    // add an id for each: title id "bookTitle" author "bookAuthor" description "bookDescription"
+    title = $("<p>").text(title);
+    // add class for each: title "bookTitle" author "bookAuthor" description "bookDescription"
+    title.addClass("bookTitle");
     // append each to the book card
+    book.append(title);
     // create cover variable with the src being bookImage
     // append to the book card
     // append book card to the apporiate div
+    $(`#${displayID}`).append(book);
 }
