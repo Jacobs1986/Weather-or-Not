5554f58012d76a29a9e338b263d56b81e768ed19
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-25 14:53:44 -0700

    I found a way to get the temperature in Fahrenheit without having to write a function to convert it. In the url link after ,us& i put in the units=imperial and it appeared in the results. Grabbed that and logged it to the console rounding it down to the closest whole number.

--- javascript/weather.js
+++ javascript/weather.js
@@ -2,34 +2,40 @@
 console.log("linked")
 var zip;
 var key = "2c2ebda745768cdab3335ed8a1f21aef"
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
     console.log("clicked");
-   zip = $("#zip").val().trim();
-   console.log(zip);
+    zip = $("#zip").val().trim();
+    console.log(zip);
 
-   var queryURL = `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${key}`;
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&units=imperial&APPID=${key}`;
 
-   
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
         console.log(response.list)
         let forecast = response.list;
         console.log(forecast);
-       for (i = 0; i < 3; i++) {
-           // get the date
-           // can we also get time? 
-           let date = forecast[i].dt;
-           // convert the date to a more readable time.
-           date = moment.unix(date).toDate();
-           console.log(moment(date).format("MM/DD/YYYY h:mm A"));
-       }
-})});
+        for (i = 0; i < 3; i++) {
+            // get the date
+            // can we also get time? 
+            let date = forecast[i].dt;
+            // convert the date to a more readable time.
+            date = moment.unix(date).toDate();
+            convertedDate = moment(date).format("h:mm A")
+            // get the temperature
+            temperature = forecast[i].main.temp;
+            // console log the results
+            console.log(convertedDate);
+            console.log(`Temperature (F): ${Math.floor(temperature)}`)
+        }
+    })
+});
 
 /*
 convert to F
 --display weather
-take in degree, weather.description to determine good or bad weather 
+take in degree, weather.description to determine good or bad weather
  */
