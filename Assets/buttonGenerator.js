8d5934cf3ee8f75b7fda22dc32c70899def5857c
chelsealuttrall <chelsealuttrall@gmail.com>
2020-01-25 13:26:30 -0700

    Co-authored-by: SamanthaCoke <samanthacokeclark@gmail.com>

---
+++ Assets/buttonGenerator.js
@@ -0,0 +1,37 @@
+goodWeather = ['hike', 'BBQ']
+
+for (let i = 0; i < goodWeather.length; i++) {
+    //create id"Activity Button" with new text and src link that appends below the other
+    //
+    //    function printBtn() {
+    //        for (var i = 0; i < goodWeather.length; i++) {
+    //            var btn = document.createElement("button");
+    //            var t = document.createTextNode(goodWeather[i]);
+    //            btn.appendChild();
+    //            document.body.appendChild(activityButton);
+    //        }
+    //    }
+    //
+    //    printBtn();
+    //}
+    //
+    //$("#activityButton").text(questionArray[currentQuestionIndex].q);
+    ////Displaying Multiple Choice Selection in HTML
+    //$("#choicesHTML").html(possibilities(currentQuestionIndex));
+    ////Putting multiple choice selection to radio buttons and displaying.
+    //function possibilities(currentQuestionIndex) {
+    //    let possiblitiesDiv = $("<div>");
+    //    for (let i = 0; i < questionArray[currentQuestionIndex].p.length; i++) {
+    //        let possibilityInput = $("<input>").attr("type", "radio").val(questionArray[currentQuestionIndex].p[i]).attr("name", "question" + currentQuestionIndex);
+    //        let possibilityLabel = $("<label>").text(questionArray[currentQuestionIndex].p[i]);
+    //        possiblitiesDiv.append(possiblityInput, possibilityLabel, "<br>")
+    //    }
+    //    return possiblitiesDiv;
+    //};
+    //
+    //let factDiv
+    //
+    ////hide fact
+    //$(factDiv).hide;
+    //
+    ////display fact
