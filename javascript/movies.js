b8e926d40ffc8f05181169420ab74c1595072245
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-26 18:56:22 -0700

    Began writing out the call for the API. Got a response and everything looks fine.

--- javascript/movies.js
+++ javascript/movies.js
@@ -1,2 +1,16 @@
 // testing to make sure things are linked
 console.log("linked");
+
+// the api key
+let key = "95804496ea455c7b61da8ba727ca99ec";
+console.log(key);
+
+// URL
+queryURL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}`;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
