3179c351491e2fcf4452a26e48b68d5b68b8fd64
chelsealuttrall <chelsealuttrall@gmail.com>
2020-01-26 21:35:08 -0700

    added accordian file stuff I need for a jumping off point

--- Assets/buttonGenerator.js
+++ Assets/buttonGenerator.js
@@ -6,6 +6,40 @@ let weatherButtonIndex = 0
 
 //this isn't working through all of them...
 for (let i = 0; i < goodWeatherButtons[weatherButtonIndex].length; i++) {
-    $("#stuffResultsButtonArea").html("<div>").text(goodWeatherButtons[weatherButtonIndex]).attr("class", "btn btn-outline-warning").attr("id", "activityButton");
+    $("#accordian").html("<div>").text(goodWeatherButtons[weatherButtonIndex]).attr("class", "btn btn-outline-warning").attr("id", "activityButton");
     weatherButtonIndex++;
 };
+
+
+
+$(".selector").accordion({
+    animate: 200
+});
+
+
+
+`
+
+<h3></h3>//h3 = first result
+<div> 
+  <p>
+  
+  </p>
+</div>//result content
+<h3></h3>//h3 = second result//
+<div>
+  <p>
+  </p>
+</div>  //result content
+
+<h3></h3>//h3 = third result//
+<div>
+  <p>
+  </p>
+</div>  //result content
+
+<h3></h3>//h3 = fourth result
+<div>
+  <p>
+  </p>
+</div>  //p = result content;
